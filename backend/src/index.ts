6478e4b868bbb4c15b349af4ad46ae3670463bbb
harumaki1220 <haruma181220@gmail.com>
2025-11-06 05:10:06 +0900

    fix(backend): 本番環境でdotenvが実行されないよう修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,9 @@ import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
 import * as dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const JWT_SECRET = process.env.JWT_SECRET;
 if (!JWT_SECRET) {
